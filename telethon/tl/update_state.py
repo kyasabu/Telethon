699d0cd02c15fcd5123fdad6933706385ed65d0d
Lonami Exo <totufals@hotmail.com>
2017-08-14 16:04:24 +0200

    Tidy up UpdatesState.update_state method

--- telethon/tl/update_state.py
+++ telethon/tl/update_state.py
@@ -13,7 +13,6 @@ class UpdateState:
 
         self._lock = RLock()
 
-    # TODO updates.Difference and updates.State
     def update_state(self, tlobject):
         if not isinstance(tlobject, TLObject):
             return
@@ -22,26 +21,26 @@ class UpdateState:
         # crc32(b'updates.State'), crc32(b'updates.Difference')
         update_types = {0x9f89304e, 0x8af52aac, 0x23df1a01, 0x20482874}
         if type(tlobject).subclass_of_id in update_types:
-            # TODO Avoid reentering the lock?
-            if hasattr(tlobject, 'updates'):
-                for update in tlobject.updates:
-                        self.update_state(update)
+            with self._lock:
+                self._update_state(tlobject)
 
-            attributes = [x for x in ('pts', 'qts', 'seq', 'date')
-                          if hasattr(tlobject, x)]
-            if attributes:
-                with self._lock:
-                    for a in attributes:
-                        setattr(self, a, getattr(tlobject, a))
+    def _update_state(self, update):
+        if hasattr(update, 'updates'):
+            for u in update.updates:
+                self._update_state(u)
 
-            elif hasattr(tlobject, 'other_updates'):
-                # Only the case of 'difference' and 'differenceSlice',
-                # these don't seem to have 'pts', 'qts', etc.
-                with self._lock:
-                    if hasattr(tlobject, 'state'):
-                        self.update_state(tlobject.state)
-                    elif hasattr(tlobject, 'intermediate_state'):
-                        self.update_state(tlobject.intermediate_state)
+        elif hasattr(update, 'other_updates'):
+            # Only the case of 'difference' and 'differenceSlice'
+            # have both 'other_updates' and 'state/intermediate_state'
+            for u in update.other_updates:
+                self._update_state(u)
 
-                    for update in tlobject.other_updates:
-                        self.update_state(update)
+            if hasattr(update, 'state'):
+                self._update_state(update.state)
+            elif hasattr(update, 'intermediate_state'):
+                self._update_state(update.intermediate_state)
+
+        # Here is where the actual updating happens
+        for a in ('pts', 'qts', 'seq', 'date'):
+            if hasattr(update, a):
+                setattr(self, a, getattr(update, a))
